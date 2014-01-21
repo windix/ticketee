6109d3e9ca9fb683f931b1a6ec2c8fbc4a9cc191
Wei Feng <windix@gmail.com>
2014-01-21 21:26:19 +1100

    Add title helper and update for show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  include ApplicationHelper
+
   def index
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ")
+      end
+    else
+      "Ticketee"
+    end
+  end
 end
