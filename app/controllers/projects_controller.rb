7e20ab6c9305c8f69eec853d5d6592cd466d19ea
Wei Feng <windix@gmail.com>
2014-01-21 20:54:48 +1100

    'Creating Projects' feature complete.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Project has been created."
+    else
+      # TODO
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+
+end
