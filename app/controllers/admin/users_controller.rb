63a7f502fb739badbe61619801cbdf94388435c0
Wei Feng <windix@gmail.com>
2014-01-26 23:18:34 +1100

    Able to create users from admin UI

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,30 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.order(:name)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, :notice => 'User has been created.'
+    else
+      flash[:alert] = 'User has not been created.'
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    # set password confirmation since we don't require that from admin UI
+    params[:user][:password_confirmation] = params[:user][:password]
+
+    params.require('user').permit(:name, :email, :password, :password_confirmation)
   end
 
 end
