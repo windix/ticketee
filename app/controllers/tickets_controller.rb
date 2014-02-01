4e56444a1ce0fa03153a6e2cabec3e38dc204e9c
Wei Feng <windix@gmail.com>
2014-02-01 23:36:23 +1100

    Restrict reading tickets to correct project scope

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,7 @@
 class TicketsController < ApplicationController
+  before_action :require_signin!
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
-  before_action :require_signin!, except: [:index, :show]
 
   def new
     @ticket = @project.tickets.build
@@ -42,9 +42,10 @@ class TicketsController < ApplicationController
   private
 
   def set_project
-    @project = Project.find(params[:project_id])
+    @project = Project.for(current_user).find(params[:project_id])
+
   rescue ActiveRecord::RecordNotFound
-    redirect_to projects_path, alert: "The project you were looking for could not be found."
+    redirect_to root_path, alert: "The project you were looking for could not be found."
   end
 
   def set_ticket
